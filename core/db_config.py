919ba7d8611805489cd975d368e839f0e573276a
neotekys <neotekys@gmail.com>
2026-04-17 13:15:40 +0900

    Update db_config.py

--- core/db_config.py
+++ core/db_config.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 DATABASE = {
-    'host': 'localhost',      # 서버 주소
-    'dbname': 'postgres',      # 데이터베이스명
+    'host': '112.10.128.29',      # 서버 주소
+    'dbname': 'ysdt',      # 데이터베이스명
     'user': 'us_gisobj',      # 사용자 아이디
     'password': 'us_gisobj', # 비밀번호
     'port': 5432
